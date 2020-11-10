02913af597233040b2640ef86f33d7a6c7b5f736
Chloe Aujoulat <chloe.aujoulat@etu.univ-lyon1.fr>
2020-11-10 20:43:00 +0100

    fonction conversion fichier VCF en CSV fonctionnelle

--- script_python.py
+++ script_python.py
@@ -1,39 +1,43 @@
-#!/usr/bin/python
+#! /usr/bin/python
 #-*-coding: utf-8-*-
 
-# Conversion du fichier au format VCF en format csv
-# le fichier csv ressemble au format BED
 
-import pandas
-import vcf
-import csv
-import sys
+# commencer par taper la commande pour convertir le fichier au format BCF en un fichier au format VCF
+# bcftools view -v output.bcf > output.vcf
+ 
+#import sys
 
-def conversion(f_entrance, f_exit):
+def conversion(f_entrance, f_exit_write):
     with open(f_entrance, "r") as file_vcf:
-        file_vcf = f_entrance.read()
-        #print(file_vcf)
+        dict_general = {}
         liste_generale = []
         for line in file_vcf:
+            print(line)
             petit_dict_vcf = {}
-            colonne = line.split("\t")
-            petit_dict_vcf["CHROM"] = colonne[0]
-            petit_dict_vcf["POS"] = colonne[1]
-            petit_dict_vcf["ID"] = colonne[2]
-            petit_dict_vcf["REF"] = colonne[3]
-            petit_dict_vcf["ALT"] = colonne[4]
-            petit_dict_vcf["QUAL"] = colonne[5]
-            petit_dict_vcf["FILTER"] = colonne[6]
-            # on ajoute le petit dictionnaire dans la liste générale
-            liste_generale.append(petit_dict_vcf)
+            if line.startswith("##"):
+                continue
+            else:
+                colonne = line.split("\t")
+                petit_dict_vcf["CHROM"] = colonne[0]
+                petit_dict_vcf["POS"] = colonne[1]
+                petit_dict_vcf["ID"] = colonne[2]
+                petit_dict_vcf["REF"] = colonne[3]
+                petit_dict_vcf["ALT"] = colonne[4]
+                petit_dict_vcf["QUAL"] = colonne[5]
+                petit_dict_vcf["FILTER"] = colonne[6]
+                # on ajoute le petit dictionnaire dans la liste générale
+                liste_generale.append(petit_dict_vcf)
         print(liste_generale)
     print("Fin de la lecture du fichier .vcf")
 
 
     # Ecriture du fichier au format csv
 
-    with open(f_exit, "w", nexline = '') as file_csv:
-        writerCSV = csv.writer(file_csv, delimiter = "\t")
-        fieldnames = ["[1]CHROM", "[2]POS", "[3]ID", "[4]REF", "[5]ALT", "[6]QUAL", "[7]FILTER"]
-        writer.writeheader()
-        writerCSV = writerow()
+    with open(f_exit_write, "w") as file_csv_write:
+        for i in liste_generale:
+            petit_dict_vcf = i
+            file_csv_write.write("{0};{1};{2};{3};{4};{5};{6}\n".format(petit_dict_vcf["ID"], petit_dict_vcf["CHROM"], petit_dict_vcf["POS"], petit_dict_vcf["REF"], petit_dict_vcf["ALT"], petit_dict_vcf["QUAL"], petit_dict_vcf["FILTER"]))
+        print(file_csv_write)
+    print("Fin de l'écriture du fichier .csv")
+print(conversion("calls.vcf", "calls.csv"))
+        
